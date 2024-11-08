d61572e8968d189244778300cad9190e8fabcbd3
Jean Francois MARTINS <jf.martins@medxsoft.com>
2024-11-08 14:35:11 +0100

    Partie1-Statis template

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Bonjour {{ firstName.toUpperCase() }}</h1>
+  <p>Comment allez vous ?</p>
+</template>
+<script setup>
+  const firstName="John";
+
+</script>
+<style>
+h1{
+  color:chartreuse;
+}
+</style>
