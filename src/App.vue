6917b7097dcafdacb90eddc30e4b2fa64f3e733d
Jean Francois MARTINS <jf.martins@medxsoft.com>
2024-11-08 14:52:52 +0100

    Increment by timer

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <h1>Bonjour {{ firstName.toUpperCase() }}</h1>
+  <p>Vous avez cliquer : {{ counter }} fois !</p>
+
   <p>Comment allez vous ?</p>
 </template>
 <script setup>
+import {ref} from 'vue';
   const firstName="John";
+  const counter = ref(1);
+  setInterval(() =>  {counter.value++},1000  );
+ 
 
 </script>
 <style>
 h1{
-  color:chartreuse;
+  color:red;
 }
 </style>
